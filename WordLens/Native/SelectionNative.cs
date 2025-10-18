74330451e38d38de0f7d28e674d1716eba04c7fb
CrimsonNinja <panruzun@gmail.com>
2025-10-18 12:49:13 +0800

    PublishAot NativeLibrary

---
+++ WordLens/Native/SelectionNative.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WordLens.Native
+{
+    internal partial class SelectionNative
+    {
+        private const string LibName = "native";
+
+        [LibraryImport(LibName, EntryPoint = "get_selection_text")]
+        private static partial IntPtr GetSelectionTextPtr();
+
+        [LibraryImport(LibName, EntryPoint = "free_c_string")]
+        private static partial void FreeCString(IntPtr ptr);
+
+        public static string GetSelectionText()
+        {
+
+            IntPtr resultPtr = IntPtr.Zero;
+            string resultString = null;
+
+            try
+            {
+                resultPtr = GetSelectionTextPtr();
+
+                if (resultPtr == IntPtr.Zero)
+                {
+                    return null;
+                }
+
+                resultString = Marshal.PtrToStringUTF8(resultPtr);
+                
+            }
+            finally
+            {
+                if (resultPtr != IntPtr.Zero)
+                {
+                    FreeCString(resultPtr);
+                }
+            }
+
+            return resultString;
+        }
+    }
+}
